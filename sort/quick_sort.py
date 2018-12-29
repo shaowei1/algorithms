fe1bf8a4872c21f68512eda6afcc1185e70b526b
lihansen <648700565@qq.com>
2018-12-29 11:51:33 +0800

    qs

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,23 +1,39 @@
 def partition(a, start, end):
-    x = a[end]
     i = start
-    for j in range(start, end - 1):
-        if a[j] <= x:
-            # i += 1
+    for j in range(start, end):
+        if a[j] <= a[end]:
             a[i], a[j] = a[j], a[i]
             i += 1
-    a[i ], a[end] = a[end], a[i ]
+    a[i], a[end] = a[end], a[i]
     return i
 
 
-def quick_sort(arr, start, end):
+def quick_sort(array, start, end):
     if start < end:
-        x = partition(arr, start, end)
-        print(x)
-        quick_sort(arr,start,x)
-        quick_sort(arr,x,end)
+        x = partition(array, start, end)
+        # print(x)
+        quick_sort(array, start, x - 1)
+        quick_sort(array, x + 1, end)
+
+
+def quick_sort1(array):
+    if len(array) < 2:
+        return array
+    base = array[0]
+    less = []
+    greater = []
+    eq = []
+    for i in array:
+        if i < base:
+            less.append(i)
+        elif i > base:
+            greater.append(i)
+        else:
+            eq.append(i)
+
+    return quick_sort1(less) + eq + quick_sort1(greater)
 
 
 a = [2, 8, 7, 1, 3, 5, 6, 4]
-quick_sort(a, 0, len(a) - 1)
-print(a)
+# quick_sort(a, 0, len(a) - 1)
+print(quick_sort1(a))
